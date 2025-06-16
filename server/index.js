0ecea48a0e589fbbda485d6c471fd5ece6ef9938
zZzombie <itgelt.t.gi@gmail.com>
2025-06-16 11:44:57 +0800

    Update .gitignore

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend working!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
